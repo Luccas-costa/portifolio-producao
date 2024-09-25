f6784d194c7d23967044e7730918c7af717b4d8a
luccas costa <lucaspcosta70@gmail.com>
2024-09-25 20:18:41 -0300

    teste

--- src/components/pages/home/products.tsx
+++ src/components/pages/home/products.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Image from 'next/image'
+import Imagem from 'public/logos/logo-na-folha.jpg'
 
 export default function Products() {
   return (
-    <div className="h-[82vh] w-full">
-      <div></div>
+    <div className="flex h-[82vh] w-full items-end">
+      <div>
+        <Image src={Imagem} alt="teste" />
+      </div>
     </div>
   )
 }
