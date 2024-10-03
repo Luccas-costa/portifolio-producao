005e942276193ac9048667aea72312a84e8e0ecb
luccas costa <lucaspcosta70@gmail.com>
2024-10-03 19:39:16 -0300

    acho que vai agora

--- src/app/testes/nuqs-url-paraments/page.tsx
+++ src/app/testes/nuqs-url-paraments/page.tsx
@@ -1,27 +1,30 @@
-import Teste001 from '@/components/pages/testes/nuqs-test/teste-001'
-import Teste002 from '@/components/pages/testes/nuqs-test/teste-002'
-import Teste003 from '@/components/pages/testes/nuqs-test/teste-003'
-import Teste004 from '@/components/pages/testes/nuqs-test/teste-004'
-import Teste005 from '@/components/pages/testes/nuqs-test/teste-005'
+// import Teste001 from '@/components/pages/testes/nuqs-test/teste-001'
+// import Teste002 from '@/components/pages/testes/nuqs-test/teste-002'
+// import Teste003 from '@/components/pages/testes/nuqs-test/teste-003'
+// import Teste004 from '@/components/pages/testes/nuqs-test/teste-004'
+// import Teste005 from '@/components/pages/testes/nuqs-test/teste-005'
 
+// export default function TesteNuqs() {
+//   return (
+//     <div className="flex h-screen flex-col gap-4 bg-zinc-900 text-zinc-200">
+//       <div className="h-full w-full rounded border border-zinc-200 p-2">
+//         <Teste001 />
+//       </div>
+//       <div className="h-full w-full rounded border border-zinc-200 p-2">
+//         <Teste002 />
+//       </div>
+//       <div className="h-full w-full rounded border border-zinc-200 p-2">
+//         <Teste003 />
+//       </div>
+//       <div className="h-full w-full rounded border border-zinc-200 p-2">
+//         <Teste004 />
+//       </div>
+//       <div className="h-full w-full rounded border border-zinc-200 p-2">
+//         <Teste005 />
+//       </div>
+//     </div>
+//   )
+// }
 export default function TesteNuqs() {
-  return (
-    <div className="flex h-screen flex-col gap-4 bg-zinc-900 text-zinc-200">
-      <div className="h-full w-full rounded border border-zinc-200 p-2">
-        <Teste001 />
-      </div>
-      <div className="h-full w-full rounded border border-zinc-200 p-2">
-        <Teste002 />
-      </div>
-      <div className="h-full w-full rounded border border-zinc-200 p-2">
-        <Teste003 />
-      </div>
-      <div className="h-full w-full rounded border border-zinc-200 p-2">
-        <Teste004 />
-      </div>
-      <div className="h-full w-full rounded border border-zinc-200 p-2">
-        <Teste005 />
-      </div>
-    </div>
-  )
+  return <div>tete</div>
 }
