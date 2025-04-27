d197c34557fe381fc5f7ec090558c94961223e90
luccas costa <lucaspcosta70@gmail.com>
2025-04-26 23:31:44 -0300

    responsive v3 new

--- src/components/pages/professional/index-desktop.tsx
+++ src/components/pages/professional/index-desktop.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React from 'react'
 
 import Hero from '@/components/pages/professional/hero'
--- src/components/pages/professional/navbar.tsx
+++ src/components/pages/professional/navbar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React from 'react'
 import logo from 'public/logos/logo-tranparente.png'
 import Image from 'next/image'
