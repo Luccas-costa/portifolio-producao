16c5d77ac855214fbb94ec1adb5e82cf83c10b07
luccas costa <lucaspcosta70@gmail.com>
2025-04-27 20:53:22 -0300

    tentando desativar zomm

--- src/components/pages/professional/iphone.tsx
+++ src/components/pages/professional/iphone.tsx
@@ -123,7 +123,7 @@ export default function Iphone() {
                         <input
                           ref={inputRef}
                           type="text"
-                          className="h-[40px] w-full bg-transparent text-xs font-light text-zinc-400 outline-none placeholder:text-neutral-400/70"
+                          className="h-[40px] w-full bg-transparent text-base font-light text-zinc-400 outline-none placeholder:text-neutral-400/70"
                           placeholder="Ask anything"
                           value={pergunta}
                           onChange={(e) => setPergunta(e.target.value)}
@@ -199,7 +199,7 @@ export default function Iphone() {
                       <input
                         ref={inputRef}
                         type="text"
-                        className="mr-[2%] h-[30px] w-[98%] bg-transparent text-xxs font-light text-zinc-300 outline-none placeholder:text-neutral-400/70"
+                        className="mr-[2%] h-[30px] w-[98%] bg-transparent text-base font-light text-zinc-300 outline-none placeholder:text-neutral-400/70"
                         placeholder="Ask anything"
                         value={pergunta}
                         onChange={(e) => setPergunta(e.target.value)}
