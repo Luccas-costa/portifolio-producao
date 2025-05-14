bccc0582590fc4f906471b2db2c5456146aed049
luccas costa <lucaspcosta70@gmail.com>
2025-05-13 21:07:38 -0300

    teste3

--- src/components/pages/professional/sections/about.tsx
+++ src/components/pages/professional/sections/about.tsx
@@ -14,7 +14,7 @@ export default function About() {
   return (
     <div className={`mt-[200px] pb-[300px] screen1200:pb-[170px]`}>
       <div className="flex h-full w-full flex-wrap justify-center gap-[20px] screen1200:flex-nowrap screen1300:gap-[100px] screen1500:ml-[150px] screen1500:justify-normal">
-        <div className="mb-[100px] flex h-[450px] w-[400px] flex-col rounded-xl bg-zinc-200 screen440:h-[470px] screen440:w-[420px] screen500:h-[480px] screen500:w-[430px]">
+        <div className="mb-[100px] flex h-[400px] w-[350px] flex-col rounded-xl bg-zinc-200 screen440:h-[450px] screen440:w-[400px] screen500:h-[480px] screen500:w-[430px]">
           <div className="mb-[-10px] pt-[2px] text-center text-sm font-light text-zinc-950">
             2025
           </div>
