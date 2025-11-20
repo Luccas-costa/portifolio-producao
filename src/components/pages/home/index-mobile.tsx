66dd755df37ecab114510be4e32d173e5f5d3df9
luccas costa <lucaspcosta70@gmail.com>
2025-11-20 20:48:21 -0300

    Atualizar o index-mobile.tsx

--- src/components/pages/home/index-mobile.tsx
+++ src/components/pages/home/index-mobile.tsx
@@ -19,7 +19,7 @@ export default function IndexMobile() {
       {/* Background */}
       <div
         style={{ zIndex: 1, pointerEvents: 'none' }}
-        className="absolute bottom-0 flex h-full w-full min-h-[100vh] flex-col items-center overflow-hidden"
+        className="absolute bottom-0 flex h-full min-h-[100vh] w-full flex-col items-center overflow-hidden"
       >
         <ShootingStars />
         <StarsBackground />
