adbe04a7694dd510b280a058217c5dfa000765e8
luccas costa <lucaspcosta70@gmail.com>
2026-03-04 18:46:11 -0300

    add new part2

--- src/app/(public)/portifolio/page.tsx
+++ src/app/(public)/portifolio/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Hero from '@/components/pages/portifolio/hero'
-import Projects from '@/components/pages/portifolio/projects'
+// import Projects from '@/components/pages/portifolio/projects'
 import IntroAnimationUse from '@/components/ui/intro/intro-animation-use'
 import React, { useEffect, useState } from 'react'
 
@@ -25,7 +25,7 @@ export default function AnimatedBackground() {
           <div className="h-full bg-[#070B0C]">
             {/* REMOVIDO o bg-[#f4f4f4] daqui */}
             <div className="h-full w-screen overflow-hidden rounded-t-[100px]">
-              <Projects />
+              {/* <Projects /> */}
             </div>
           </div>
         </>
